3ccab3a8102f7dc56a7dea70b1531695de87cb83
Matthew Thomas <malex@orbiter.me>
2017-10-29 14:18:48 -0700

    Modifies build scripts to output to the .dist folder.

--- build-scripts/build-settings.sh
+++ build-scripts/build-settings.sh
@@ -6,8 +6,10 @@ set -o pipefail
 
 # Static settings
 PACKAGES_FOLDER='packages'                                          # Defines the name of the folder where build dependencies should be cached.
+RELEASE_OUTPUT_FOLDER='.dist'                                       # The name of the folder where a release for distribution is stored.
 
 # Calculated settings
-export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"              # Specifies the full local path to the packages folder.
-export SHELL_SCRIPTS_PATH="$(readlink -f ./build-scripts)"          # Specifies the full local path to the shell scripts folder.
-export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)" # Specifies the full local path to the node shell tools.
+export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"               # Specifies the full local path to the packages folder.
+export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_FOLDER)"   # Specifies the full local path to the release output folder.
+export SHELL_SCRIPTS_PATH="$(readlink -f ./build-scripts)"           # Specifies the full local path to the shell scripts folder.
+export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)"  # Specifies the full local path to the node shell tools.
