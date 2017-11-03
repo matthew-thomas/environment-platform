a5f14710a5f735bc03d7c8b5afebdef8e6ea1719
Matthew Thomas <malex@orbiter.me>
2017-11-02 22:06:14 -0700

    Modifies build-settings to auto-create the distribution folder and calculate the release output path incorporating the build number.

--- build-scripts/build-settings.sh
+++ build-scripts/build-settings.sh
@@ -6,10 +6,21 @@ set -o pipefail
 
 # Static settings
 PACKAGES_FOLDER='.packages'                                         # Defines the name of the folder where build dependencies should be cached.
-RELEASE_OUTPUT_FOLDER='.dist'                                       # The name of the folder where a release for distribution is stored.
+DISTRIBUTIONS_FOLDER='.dist'                                        # The name of the folder where release distributions are stored.
 
 # Calculated settings
-export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"               # Specifies the full local path to the packages folder.
-export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_FOLDER)"   # Specifies the full local path to the release output folder.
-export SHELL_SCRIPTS_PATH="$(readlink -f ./build-scripts)"           # Specifies the full local path to the shell scripts folder.
-export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)"  # Specifies the full local path to the node shell tools.
+export DISTRIBUTIONS_PATH="$(readlink -f $DISTRIBUTIONS_FOLDER)"          # Specifies the full local path to the dist folder.
+export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"                    # Specifies the full local path to the packages folder.
+export SHELL_SCRIPTS_PATH="$(readlink -f ./build-scripts)"                # Specifies the full local path to the shell scripts folder.
+export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)"       # Specifies the full local path to the node shell tools.
+export VERSION_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/version.sh)" # Specifies the full local path to the version tools.
+
+mkdir -p $DISTRIBUTIONS_PATH                                              # Ensure the distributions folder exists
+
+# Load the app.build version
+source $VERSION_TOOLS_PATH
+export BUILD_NUMBER="$(get_build_number)"
+echo "BUILD_NUMBER: $BUILD_NUMBER"
+
+RELEASE_OUTPUT_RELATIVE_PATH="$DISTRIBUTIONS_PATH/$BUILD_NUMBER"
+export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_RELATIVE_PATH)" # Specifies the full local path to the release output folder.
---
+++ build-scripts/version.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# This script provides functions for working with version numbers.
+
+set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+
+get_app_version() {
+    # TODO: Get version from package.json
+    APP_VERSION="0.1"
+    
+    # Get build number
+    BUILD_NUMBER="$(get_build_number)"
+
+    echo "$APP_VERSION.$BUILD_NUMBER"
+}
+
+get_build_number() {
+    # If the current build number isn't specified by an external agent (CI server),
+    # then calculate it based on the previous build number.
+    BUILD_NUMBER="" # TODO: Integrate with CI server.
+    if [[ -z "$BUILD_NUMBER" ]]; then
+        LAST_BUILD_NUMBER="$(ls -l $DISTRIBUTIONS_FOLDER | tail -1 | grep -Po '\d+$')"
+
+        if [[ -z "$LAST_BUILD_NUMBER" ]]; then
+            # This is the first build.
+            LAST_BUILD_NUMBER=0
+        fi
+
+        BUILD_NUMBER=$((LAST_BUILD_NUMBER+1))
+    fi
+
+    echo $BUILD_NUMBER
+}
