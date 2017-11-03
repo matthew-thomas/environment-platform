4d40f6b9b160c4eb325862aca219914dcf4a808a
Matthew Thomas <malex@orbiter.me>
2017-11-02 22:01:54 -0700

    Adds basic script sets to the build.sh.

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+# Set some bash execution settings...
+set -o errexit
+set -o nounset
+set -o pipefail
+
 echo Provisioning Node.js modules...
 npm install
 echo Done!
+
 echo
 mkdir -p $RELEASE_OUTPUT_PATH
 echo Release distribution folder \'$RELEASE_OUTPUT_PATH\' created/exists!
+
 echo "Hello, World!" > "$RELEASE_OUTPUT_PATH/hello-world.txt"
