b43901ceb4f79535e61ec9dfa41205dd73f02ccb
Matthew Thomas <malex@orbiter.me>
2018-01-19 20:29:36 -0800

    Relocate non setting code out of the build-settings file.

--- build-scripts/build-settings.sh
+++ build-scripts/build-settings.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 # Static settings
-PACKAGES_FOLDER='.packages'                                         # Defines the name of the folder where build dependencies should be cached.
-DISTRIBUTIONS_FOLDER='.dist'                                        # The name of the folder where release distributions are stored.
+export PACKAGES_FOLDER='.packages'                                         # Defines the name of the folder where build dependencies should be cached.
+export DISTRIBUTIONS_FOLDER='.dist'                                        # The name of the folder where release distributions are stored.
 
 # Calculated settings
 export DISTRIBUTIONS_PATH="$(readlink -f $DISTRIBUTIONS_FOLDER)"          # Specifies the full local path to the dist folder.
@@ -14,13 +14,3 @@ export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"                    # Spec
 export SHELL_SCRIPTS_PATH="$(readlink -f ./build-scripts)"                # Specifies the full local path to the shell scripts folder.
 export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)"       # Specifies the full local path to the node shell tools.
 export VERSION_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/version.sh)" # Specifies the full local path to the version tools.
-
-mkdir -p $DISTRIBUTIONS_PATH                                              # Ensure the distributions folder exists
-
-# Load the app.build version
-source $VERSION_TOOLS_PATH
-export BUILD_NUMBER="$(get_build_number)"
-echo "BUILD_NUMBER: $BUILD_NUMBER"
-
-RELEASE_OUTPUT_RELATIVE_PATH="$DISTRIBUTIONS_PATH/$BUILD_NUMBER"
-export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_RELATIVE_PATH)" # Specifies the full local path to the release output folder.
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -9,8 +9,20 @@ echo Provisioning Node.js modules...
 npm install
 echo Done!
 
+mkdir -p $DISTRIBUTIONS_PATH                                              # Ensure the distributions folder exists
+
+# Load the app.build version
+source $VERSION_TOOLS_PATH
+export BUILD_NUMBER="$(get_build_number)"
+echo "BUILD_NUMBER: $BUILD_NUMBER"
+export APP_VERSION="$(get_app_version)"
+echo "APP_VERSION: $APP_VERSION"
+
+RELEASE_OUTPUT_RELATIVE_PATH="$DISTRIBUTIONS_PATH/$BUILD_NUMBER"
+export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_RELATIVE_PATH)" # Specifies the full local path to the release output folder.
+
 echo
 mkdir -p $RELEASE_OUTPUT_PATH
 echo Release distribution folder \'$RELEASE_OUTPUT_PATH\' created!
 
-echo "Hello, World!" > "$RELEASE_OUTPUT_PATH/hello-world.txt"
+echo "Hello, World! $APP_VERSION" > "$RELEASE_OUTPUT_PATH/hello-world.txt"
