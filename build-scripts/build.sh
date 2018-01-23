265a1f50aa2209d5d3dd8fcc51f49d148e028c8a
Matthew Thomas <malex@orbiter.me>
2018-01-22 23:23:19 -0800

    Updates build script formatting.

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -13,10 +13,11 @@ mkdir -p $DISTRIBUTIONS_PATH                                              # Ensu
 
 # Load the app.build version
 source $VERSION_TOOLS_PATH
+echo
 export BUILD_NUMBER="$(get_build_number)"
 echo "BUILD_NUMBER: $BUILD_NUMBER"
 export APP_VERSION="$(get_app_version)"
-echo "APP_VERSION: $APP_VERSION"
+echo "APP_VERSION:  $APP_VERSION"
 
 RELEASE_OUTPUT_RELATIVE_PATH="$DISTRIBUTIONS_PATH/$BUILD_NUMBER"
 export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_RELATIVE_PATH)" # Specifies the full local path to the release output folder.
