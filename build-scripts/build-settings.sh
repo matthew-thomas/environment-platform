8cb440ee67215ab5d85e32bf855d029cd3510d84
Matthew Thomas <malex@orbiter.me>
2018-01-27 15:45:30 -0800

    Adds initial deploy script and updates build script to add it to the dist.

--- build-scripts/build-settings.sh
+++ build-scripts/build-settings.sh
@@ -7,6 +7,7 @@ set -o pipefail
 # Static settings
 export PACKAGES_FOLDER='.packages'                                         # Defines the name of the folder where build dependencies should be cached.
 export DISTRIBUTIONS_FOLDER='.dist'                                        # The name of the folder where release distributions are stored.
+export SOURCE_FOLDER='src'                                                 # The name of the folder containing all the source code.
 
 # Calculated settings
 export DISTRIBUTIONS_PATH="$(readlink -f $DISTRIBUTIONS_FOLDER)"          # Specifies the full local path to the dist folder.
@@ -14,3 +15,4 @@ export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"                    # Spec
 export SHELL_SCRIPTS_PATH="$(readlink -f ./build-scripts)"                # Specifies the full local path to the shell scripts folder.
 export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)"       # Specifies the full local path to the node shell tools.
 export VERSION_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/version.sh)" # Specifies the full local path to the version tools.
+export SOURCE_PATH="$(readlink -f $SOURCE_FOLDER)"                        # Specifies the full local path to the source code.
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -9,15 +9,15 @@ echo Provisioning Node.js modules...
 npm install
 echo Done!
 
-mkdir -p $DISTRIBUTIONS_PATH                                              # Ensure the distributions folder exists
+# Ensure the distributions folder exists
+mkdir -p $DISTRIBUTIONS_PATH
 
 # Load the app.build version
 source $VERSION_TOOLS_PATH
 echo
 export BUILD_NUMBER="$(get_build_number)"
-echo "BUILD_NUMBER: $BUILD_NUMBER"
 export APP_VERSION="$(get_app_version)"
-echo "APP_VERSION:  $APP_VERSION"
+echo "BUILD_NUMBER: $BUILD_NUMBER"
 
 RELEASE_OUTPUT_RELATIVE_PATH="$DISTRIBUTIONS_PATH/$BUILD_NUMBER"
 export RELEASE_OUTPUT_PATH="$(readlink -f $RELEASE_OUTPUT_RELATIVE_PATH)" # Specifies the full local path to the release output folder.
@@ -27,3 +27,8 @@ mkdir -p $RELEASE_OUTPUT_PATH
 echo Release distribution folder \'$RELEASE_OUTPUT_PATH\' created!
 
 echo "Hello, World! $APP_VERSION" > "$RELEASE_OUTPUT_PATH/hello-world.txt"
+cp $SOURCE_PATH/deployment/*.* $RELEASE_OUTPUT_PATH
+
+echo
+echo Release $APP_VERSION completed!
+echo
