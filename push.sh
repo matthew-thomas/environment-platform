4850dfd826200cc59e9802d03ba9e5b500ad7a4f
Matthew Thomas <malex@orbiter.me>
2018-01-27 21:46:13 -0800

    Adds pull and push script sections to the package.json invokable via npm.
    Main interface via push.sh.

---
+++ push.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Set some bash execution settings...
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# TODO: Fail if any pending changes
+
+BUILD_SCRIPTS_PATH='./build-scripts'
+source ./build-scripts/build-settings.sh
+echo Build settings loaded!
+
+source $NODE_TOOLS_PATH
+echo Script $NODE_TOOLS_PATH loaded!
+
+initialize_node 'v6.3.1' $PACKAGES_PATH
+
+npm run push
