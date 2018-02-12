93ca7d6342f55c22432287b53022f539a9151f53
Matthew Thomas <malex@orbiter.me>
2018-02-11 22:15:43 -0800

    Adds very basic flow for the deploy script.

---
+++ src/deployment/azure.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Set some bash execution settings...
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo Deployed to Azure!
--- src/deployment/deploy.sh
+++ src/deployment/deploy.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$(dirname "$0")
+# Set some bash execution settings...
+set -o errexit
+set -o nounset
+set -o pipefail
 
+SCRIPT_PATH=$(dirname "$0")
+PACKAGES_PATH=$SCRIPT_PATH/.packages
+DEPLOY_SCRIPTS_PATH=$SCRIPT_PATH
+
+if [ ! -d $PACKAGES_PATH ]; then
+    echo "Deployment dependencies $PACKAGES_PATH does not exist, running setup..."
+
+    $DEPLOY_SCRIPTS_PATH/install-deploy-tools.sh
+fi
+
+SCRIPT_PATH=$(dirname "$0")
 VERSION=`cat $SCRIPT_PATH/version.txt`
+ENVIRONMENT_PLATFORM=azure
+
+echo Environment Platform: $ENVIRONMENT_PLATFORM
+
+$DEPLOY_SCRIPTS_PATH/$ENVIRONMENT_PLATFORM.sh
+
 echo Deployment of $VERSION Completed!
